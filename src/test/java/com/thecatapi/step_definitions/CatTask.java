56c4a9084cd0cca18b93704facdd27fdaa2616cb
Cemil5998 <85905072+Cemil5998@users.noreply.github.com>
2022-01-26 20:36:16 +0300

    almost done

--- src/test/java/com/thecatapi/step_definitions/CatTask.java
+++ src/test/java/com/thecatapi/step_definitions/CatTask.java
@@ -1,85 +1,128 @@
 package com.thecatapi.step_definitions;
 
+import com.thecatapi.utilities.CatUtils;
+import com.thecatapi.utilities.ConfigurationReader;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import org.junit.Assert;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Random;
+
+import static io.restassured.RestAssured.given;
 
 public class CatTask {
+    String url = ConfigurationReader.get("uri");
+    Response response;
+    List<Map<String ,Object>> allVoteList;
+    Map<String ,Object> randomElement;
+    Integer idToKeep;
+
     @When("the user gets votes information using token")
     public void the_user_gets_votes_information_using_token() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new cucumber.api.PendingException();
+        response = given().accept(ContentType.JSON)
+                .header("x-api-key", "DEMO-API-KEY")
+                .when().get(url);
     }
 
-    @Then("status code should be {string}")
-    public void status_code_should_be(String string) {
-        // Write code here that turns the phrase above into concrete actions
-        throw new cucumber.api.PendingException();
+    @Then("status code should be {int}")
+    public void status_code_should_be(int code) {
+        Assert.assertEquals(code, response.statusCode());
     }
 
     @Then("length of the response result is more than {int}")
     public void length_of_the_response_result_is_more_than(Integer int1) {
-        // Write code here that turns the phrase above into concrete actions
-        throw new cucumber.api.PendingException();
+        Assert.assertTrue(response.header("Content-Length").length() > 0);
     }
 
     @Given("the user gets all votes")
     public void the_user_gets_all_votes() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new cucumber.api.PendingException();
+        the_user_gets_votes_information_using_token();
+        allVoteList = response.body().as(List.class);
+      //  System.out.println(allVoteList);
     }
+
 
     @When("the user selects a random element")
     public void the_user_selects_a_random_element() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new cucumber.api.PendingException();
+        Random random = new Random();
+        int rnNum = random.nextInt(allVoteList.size());
+        randomElement = allVoteList.get(rnNum);
+        double idDouble = (double)randomElement.get("id");
+        int id = (int) idDouble;
+        response = given().accept(ContentType.JSON)
+                .header("x-api-key", "DEMO-API-KEY")
+                .when().get(url + "/"+id);
+        Assert.assertEquals(200, response.statusCode());
+        response.prettyPrint();
+        System.out.println("randomElement : " + randomElement);
     }
 
     @Then("all field in response object match the selected element")
     public void all_field_in_response_object_match_the_selected_element() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new cucumber.api.PendingException();
+        Map<String ,Object> actual = response.body().as(Map.class);
+        actual.remove("user_id");
+        Assert.assertEquals(randomElement, actual);
     }
 
-    @When("the user create a new vote with image id {string} and sub id {string} and value {string}")
-    public void the_user_create_a_new_vote_with_image_id_and_sub_id_and_value(String string, String string2, String string3) {
-        // Write code here that turns the phrase above into concrete actions
-        throw new cucumber.api.PendingException();
+    @When("the user create a new vote with image id {string} and sub id {string} and value {int}")
+    public void the_user_create_a_new_vote_with_image_id_and_sub_id_and_value(String imgId, String subId,
+                                                                              int value) {
+        Map<String, Object> postMap = new HashMap<>();
+        postMap.put("image_id", imgId);
+        postMap.put("sub_id", subId);
+        postMap.put("value", value);
+
+        response = given() //.log().all()
+                .accept(ContentType.JSON)
+                .header("x-api-key", "DEMO-API-KEY")
+                .and().contentType(ContentType.JSON)
+                .and().body(postMap)
+                .when()
+                .post(url);
+      //  response.body().prettyPrint();
     }
 
     @Then("body response has {string} message")
-    public void body_response_has_message(String string) {
-        // Write code here that turns the phrase above into concrete actions
-        throw new cucumber.api.PendingException();
+    public void body_response_has_message(String expectedMessage) {
+        String actual = response.path("message");
+      //  System.out.println("actual = " + actual);
+        Assert.assertEquals(expectedMessage, actual);
     }
 
-    @Then("id respones is not empty")
+    @Then("id response is not empty")
     public void id_respones_is_not_empty() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new cucumber.api.PendingException();
+        Integer actual = response.path("id");
+        System.out.println("actual id = " + actual);
+        Assert.assertTrue(actual > 0);
     }
 
     @Given("the user creates a new vote and gets its id")
     public void the_user_creates_a_new_vote_and_gets_its_id() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new cucumber.api.PendingException();
+        the_user_create_a_new_vote_with_image_id_and_sub_id_and_value("asf2", "my-user-1234", 1);
+        Assert.assertEquals(200, response.statusCode());
     }
 
     @When("the user deletes this id")
     public void the_user_deletes_this_id() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new cucumber.api.PendingException();
+        Integer id = response.path("id");
+        CatUtils.deleteId(id);
     }
 
     @Given("the user deletes a vote and keeps its id")
     public void the_user_deletes_a_vote_and_keeps_its_id() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new cucumber.api.PendingException();
+        the_user_create_a_new_vote_with_image_id_and_sub_id_and_value("asf2", "my-user-1234", 1);
+        idToKeep = response.path("id");
+        CatUtils.deleteId(idToKeep);
     }
 
     @When("the user tries to delete this id again")
     public void the_user_tries_to_delete_this_id_again() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new cucumber.api.PendingException();
+        CatUtils.deleteId(idToKeep);
     }
 }
---
+++ src/test/java/com/thecatapi/utilities/CatUtils.java
@@ -0,0 +1,17 @@
+package com.thecatapi.utilities;
+
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+
+import static io.restassured.RestAssured.given;
+
+public class CatUtils {
+    public static Response deleteId(int id){
+        System.out.println("id to delete = " + id);
+        return given()
+                  .header("x-api-key", "DEMO-API-KEY")
+                  .accept(ContentType.JSON)
+                  .when()
+                  .delete(ConfigurationReader.get("uri")+"/"+id);
+    }
+}
